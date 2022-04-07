854005e94129f6846a1921ff3d5f09290b6d3ca9
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-07 11:31:23 -0300

    add morgan to logs

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
+const morgan = require('morgan');
+
 const movies = require('./routes/movies');
 const music = require('./routes/music');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(morgan('dev'));
 
 app.use('/movies', movies);
 app.use('/music', music);
