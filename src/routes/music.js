6d02915dd843818e3845f28a4352808176631d4a
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-09 13:06:34 -0300

    crud music/movies done

--- src/routes/music.js
+++ src/routes/music.js
@@ -1,18 +1,43 @@
 const express = require('express');
 const router = express.Router();
 
+const db = require('../db');
+
 router.get('/', (req, res) => {
-	res.status(200).send({
-		message: 'Get all musics',
+	const sql = 'SELECT * FROM music';
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Error to get all musics',
+				error: err,
+			});
+		}
+
+		res.status(200).send({
+			message: 'Get all musics',
+			result,
+		});
 	});
 });
 
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send({
-		message: `Get music by id - ${id}`,
-		id,
+	const sql = 'SELECT * FROM music WHERE music_id = ?';
+
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'No music was found',
+				err,
+			});
+		}
+
+		res.status(200).send({
+			message: `Get music - id: ${id}`,
+			result,
+		});
 	});
 });
 
@@ -24,18 +49,77 @@ router.post('/', (req, res) => {
 		price: req.body.price,
 	};
 
-	res.status(200).send({
-		message: 'Post music',
-		music,
+	const sql =
+		'INSERT INTO music (music_title, music_category, music_artist, music_price) VALUES (?,?,?,?)';
+
+	db.query(
+		sql,
+		[music.title, music.category, music.artist, music.price],
+		(err, result) => {
+			if (err) {
+				return res.status(500).send({
+					message: 'Data DID NOT inserted!',
+					error: err,
+				});
+			}
+
+			res.status(200).send({
+				message: 'Insert data success!',
+				music,
+			});
+		}
+	);
+});
+
+router.patch('/:id', (req, res) => {
+	const id = req.params.id;
+	const { title } = req.body;
+	const { category } = req.body;
+	const { artist } = req.body;
+	const { price } = req.body;
+
+	const sql =
+		'UPDATE music SET ' +
+		'music_title= ?, music_category = ?, music_artist = ?, music_price = ? ' +
+		'WHERE music_id = ?';
+
+	db.query(sql, [title, category, artist, price, id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'DID NOT updated',
+				err,
+			});
+		}
+		res.status(200).send({
+			message: 'Success',
+			music: {
+				id,
+				title,
+				category,
+				artist,
+				price,
+			},
+		});
 	});
 });
 
 router.delete('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send({
-		message: `Delete music by id - ${id}`,
-		id,
+	const sql = 'DELETE FROM music WHERE music_id = ?';
+
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Data DID NOT deleted',
+				err,
+			});
+		}
+
+		res.status(200).send({
+			message: `Delete music - id: ${id}`,
+			id,
+		});
 	});
 });
 
