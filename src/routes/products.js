c332093349224c41534063d010c66c25d548017e
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-12 11:29:29 -0300

    crud products done

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,29 +1,165 @@
 const router = require('express').Router();
 
+const db = require('../db');
+
 router.get('/', (req, res) => {
-	res.status(200).send('GET ALL');
+	const sql = 'SELECT * FROM products';
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Error',
+				err,
+			});
+		}
+
+		res.status(200).send({
+			message: 'Success. GET all products',
+			result,
+		});
+	});
 });
 
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
+	const sql = 'SELECT * FROM products WHERE cod = ?';
 
-	res.status(200).send('GET 1');
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Error',
+				err,
+			});
+		}
+
+		res.status(200).send({
+			message: `Success. GET products - cod ${id}`,
+			result,
+		});
+	});
 });
 
 router.post('/', (req, res) => {
-	res.status(200).send('POST');
+	const product = {
+		name: req.body.name,
+		trademark: req.body.trademark,
+		category: req.body.category,
+		available: req.body.available,
+		price: req.body.price,
+		url_img: req.body.url_img || '',
+	};
+
+	if (
+		!product.name ||
+		!product.trademark ||
+		!product.category ||
+		!product.available ||
+		!product.price
+	) {
+		return res.status(400).send({
+			message:
+				'At least Name, Trademark, Category, Available, Price MUST BE filled!',
+		});
+	}
+
+	const sql =
+		'INSERT INTO products (name, trademark, category, available, price, url_img) VALUES (?,?,?,?,?,?)';
+
+	db.query(
+		sql,
+		[
+			product.name,
+			product.trademark,
+			product.category,
+			product.available,
+			product.price,
+			product.url_img,
+		],
+		(err, result) => {
+			if (err) {
+				return res.status(500).send({
+					message: 'ERROR! Product DID NOT inserted on db',
+					err,
+				});
+			}
+			res.status(200).send({
+				message: 'Success. Product Inserted.',
+				product_id: result.insertId,
+			});
+		}
+	);
 });
 
 router.patch('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send('PATCH');
+	const product = {
+		name: req.body.name,
+		trademark: req.body.trademark,
+		category: req.body.category,
+		available: req.body.available,
+		price: req.body.price,
+		url_img: req.body.url_img || '',
+	};
+
+	if (
+		!product.name ||
+		!product.trademark ||
+		!product.category ||
+		!product.available ||
+		!product.price
+	) {
+		return res.status(400).send({
+			message:
+				'At least Name, Trademark, Category, Available, Price MUST BE filled!',
+		});
+	}
+
+	const sql =
+		'UPDATE products SET name = ?, trademark = ?, category = ?, available = ?, price = ?, url_img = ? WHERE cod = ?';
+
+	db.query(
+		sql,
+		[
+			product.name,
+			product.trademark,
+			product.category,
+			product.available,
+			product.price,
+			product.url_img,
+			id,
+		],
+		(err, result) => {
+			if (err) {
+				return res.status(500).send({
+					message: `ERROR! Product ${id} DID NOT updated`,
+					err,
+				});
+			}
+			res.status(200).send({
+				message: `Success! Product ${id} updated`,
+				log: result.message,
+			});
+		}
+	);
 });
 
 router.delete('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send('DELETE');
+	const sql = 'DELETE FROM products WHERE cod = ?';
+
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'ERROR! Product DID NOT deleted',
+				err,
+			});
+		}
+		res.status(200).send({
+			message: 'Success. Product deleted',
+		});
+	});
 });
 
 module.exports = router;
