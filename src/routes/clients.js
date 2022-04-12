78f2c667ffec1763006b9f86da495a28dc2ec298
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-12 12:02:35 -0300

    crud clients done

--- src/routes/clients.js
+++ src/routes/clients.js
@@ -1,29 +1,144 @@
 const router = require('express').Router();
 
+const db = require('../db');
+
 router.get('/', (req, res) => {
-	res.status(200).send('GET ALL');
+	const sql = 'SELECT * FROM clients';
+
+	db.query(sql, (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Error',
+				err,
+			});
+		}
+		res.status(200).send({
+			message: 'Success. GET all clients.',
+			result,
+		});
+	});
 });
 
 router.get('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send('GET 1');
+	const sql = 'SELECT * FROM clients WHERE cod = ?';
+
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Error',
+				err,
+			});
+		}
+		res.status(200).send({
+			message: `Success. GET client cod ${id}.`,
+			result,
+		});
+	});
 });
 
 router.post('/', (req, res) => {
-	res.status(200).send('POST');
+	const client = {
+		name: req.body.name,
+		ID: req.body.ID,
+		zipcode: req.body.zipcode,
+		country: req.body.country,
+		phone: req.body.phone,
+	};
+
+	if (
+		!client.name ||
+		!client.ID ||
+		!client.zipcode ||
+		!client.country ||
+		!client.phone
+	) {
+		return res.status(400).send({
+			message: 'All fields MUST BE filled.',
+		});
+	}
+
+	const sql =
+		'INSERT INTO clients (name, ID, zipcode, country, phone) VALUES (?,?,?,?,?)';
+
+	db.query(
+		sql,
+		[client.name, client.ID, client.zipcode, client.country, client.phone],
+		(err, result) => {
+			if (err) {
+				return res.status(500).send({
+					message: 'Client DID NOT insert on db',
+				});
+			}
+			res.status(200).send({
+				message: 'Success. Client inserted on db',
+				client_id: result.insertID,
+			});
+		}
+	);
 });
 
 router.patch('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send('PATCH');
+	const client = {
+		name: req.body.name,
+		ID: req.body.ID,
+		zipcode: req.body.zipcode,
+		country: req.body.country,
+		phone: req.body.phone,
+	};
+
+	if (
+		!client.name ||
+		!client.ID ||
+		!client.zipcode ||
+		!client.country ||
+		!client.phone
+	) {
+		return res.status(400).send({
+			message: 'All fields MUST BE filled.',
+		});
+	}
+
+	const sql =
+		'UPDATE clients SET name = ?, ID = ?, zipcode = ?, country = ?, phone = ? WHERE cod = ?';
+
+	db.query(
+		sql,
+		[client.name, client.ID, client.zipcode, client.country, client.phone, id],
+		(err, result) => {
+			if (err) {
+				return res.status(500).send({
+					message: 'ERROR. Client DID NOT update.',
+					err,
+				});
+			}
+			res.status(200).send({
+				message: 'Success. Client updated.',
+				log: result.message,
+			});
+		}
+	);
 });
 
 router.delete('/:id', (req, res) => {
 	const id = req.params.id;
 
-	res.status(200).send('DELETE');
+	const sql = 'DELETE FROM clients WHERE cod = ?';
+
+	db.query(sql, [id], (err, result) => {
+		if (err) {
+			return res.status(500).send({
+				message: 'Client DID NOT delete',
+				err,
+			});
+		}
+		res.status(200).send({
+			message: 'Success. Client deleted',
+		});
+	});
 });
 
 module.exports = router;
